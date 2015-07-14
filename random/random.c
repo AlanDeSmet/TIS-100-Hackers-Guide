9f566087f875a94ec16094e36ee65771022624fc
Alan De Smet <chaos@highprogrammer.com>
2015-07-13 20:04:22 -0500

    Working on finding new seeds

--- random/random.c
+++ random/random.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <limits.h>
 
 /*
  * This is based on ran3 from Numerical Methods in C, Second Edition
@@ -56,14 +57,48 @@ int32_t math_random(int32_t mini, int32_t maxi) {
 	return floor(f * range) + mini;
 }
 
-int main(int argc, char * argv[]) {
-	int i = 0;
-	float f;
-	int32_t out;
-	math_randomseed(55847854);
+void run_test(int32_t seed) {
+	int i;
+	math_randomseed(seed);
 	for(i=0; i < 39; i++) { printf("%d\n", math_random(1,3)); }
 	for(i=0; i < 39; i++) { printf("%d\n", math_random(1,999)); }
 	for(i=0; i < 39; i++) { printf("%d\n", math_random(-3,-1)); }
 	for(i=0; i < 39; i++) { printf("%d\n", math_random(-99,999)); }
+}
+
+int validate(int32_t seed) {
+	int i;
+	int results[] = { 2, 1, 3, 2, 2, 3, 3, 2, 3, 3, 3, 2, 1, 2, 1, 2, 1, 3, 3, 2, 3, 3, 2, 2, 2, 1, 1, 2, 1, 1, 2, 1, 3, 2, 1, 2, 3, 3, 3 };
+	math_randomseed(seed);
+	for(i=0; i < 39; i++) {
+		int32_t this = math_random(1,3);
+		if(this != results[i]) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+#define REPORT_FREQ 100000
+
+int main(int argc, char * argv[]) {
+	int i = 0;
+	float f;
+	int32_t out;
+
+	for(i = 0; i < MBIG; i++) {
+		if((i%REPORT_FREQ) == 0) {
+			float portion = (float)i / (float)MBIG;
+			printf("%d (%.2f%%)\n", i, portion*100.0);
+		}
+		if(validate(i)) {
+			printf("The seed is %d\n", i);
+			return 0;
+		}
+	}
+
+	validate(55847854);
+
+	/* run_test(55847854); */ /* Pre-July 13 */
 	return 0;
 }
